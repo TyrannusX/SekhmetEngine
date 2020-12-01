e28d2e341c03ab869820072fd4e58ceead718839
Robert Reyes <neorob22@gmail.com>
2020-11-30 23:40:21 -0600

    replacing bgfx with vulkan

--- GameEngine/Include/Graphics/Graphics.h
+++ GameEngine/Include/Graphics/Graphics.h
@@ -9,83 +9,26 @@
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 #include <enkiTS/TaskScheduler.h>
-#include <bgfx/bgfx.h>
+#include <vulkan/vulkan.h>
 #include "Components/StaticMeshComponent.h"
 #include "Entity/Entity.h"
 
 namespace SekhmetEngine
 {
-	struct GraphicsCallback : public bgfx::CallbackI
-	{
-		virtual void fatal(const char* _filePath, uint16_t _line, bgfx::Fatal::Enum _code, const char* _str) override
-		{
-			std::cout << "BGFX FATAL ERROR " << _str << std::endl;
-		}
-
-		virtual void traceVargs(const char* _filePath, uint16_t _line, const char* _format, va_list _argList) override
-		{
-		}
-
-		virtual void profilerBegin(const char* /*_name*/, uint32_t /*_abgr*/, const char* /*_filePath*/, uint16_t /*_line*/) override
-		{
-		}
-
-		virtual void profilerBeginLiteral(const char* /*_name*/, uint32_t /*_abgr*/, const char* /*_filePath*/, uint16_t /*_line*/) override
-		{
-		}
-
-		virtual void profilerEnd() override
-		{
-		}
-
-		virtual uint32_t cacheReadSize(uint64_t _id) override
-		{
-			return 0;
-		}
-
-		virtual bool cacheRead(uint64_t _id, void* _data, uint32_t _size) override
-		{
-			return true;
-		}
-
-		virtual void cacheWrite(uint64_t _id, const void* _data, uint32_t _size) override
-		{
-		}
-
-		virtual void screenShot(const char* _filePath, uint32_t _width, uint32_t _height, uint32_t _pitch, const void* _data, uint32_t /*_size*/, bool _yflip) override
-		{
-		}
-
-		virtual void captureBegin(uint32_t _width, uint32_t _height, uint32_t /*_pitch*/, bgfx::TextureFormat::Enum /*_format*/, bool _yflip) override
-		{
-		}
-
-		virtual void captureEnd() override
-		{
-		}
-
-		virtual void captureFrame(const void* _data, uint32_t /*_size*/) override
-		{
-		}
-	};
-
 	class Graphics : public enki::ITaskSet
 	{
 		private:
 			int width;
 			int height;
 			std::vector<Entity*> entities;
-			bgfx::ShaderHandle vertexShader;
-			bgfx::ShaderHandle fragmentShader;
-			bgfx::ProgramHandle programHandle;
-			bgfx::VertexBufferHandle vertexBufferHandle;
+			VkInstance vulkanInstance;
 		public:
 			void Initialize(GLFWwindow* renderTargetWindow, std::vector<Entity*> entitiesIn);
 			void Render();
 			void Update();
 			void Destroy();
 			void SetViewRectSize(int widthIn, int heightIn);
-			bgfx::ShaderHandle LoadShader(std::string shaderPath);
+			void LoadShader(std::string shaderPath);
 			void ExecuteRange(enki::TaskSetPartition range_, uint32_t threadnum_) override;
 	};
 }
--- GameEngine/Sources/Graphics/Graphics.cpp
+++ GameEngine/Sources/Graphics/Graphics.cpp
@@ -3,10 +3,6 @@
 * License file: https://github.com/TyrannusX/SekhmetEngine/blob/main/LICENSE
 */
 #include <fstream>
-#include <bgfx/bgfx.h>
-#include <bx/bx.h>
-#include <bx/math.h>
-#include <bgfx/platform.h>
 #include <glm/vec3.hpp>
 #include <glm/vec2.hpp>
 #include <glm/mat4x4.hpp>
@@ -20,90 +16,50 @@ namespace SekhmetEngine
 {
 	void Graphics::Initialize(GLFWwindow* renderTargetWindow, std::vector<Entity*> entitiesIn)
 	{
-		//initial render call to tell bgfx to use the same thread as the window thread
-		bgfx::renderFrame();
-
+		//Get the current window size
 		glfwGetWindowSize(renderTargetWindow, &width, &height);
 
-		//initialize bgfx (renderer)
-		bgfx::Init init;
-		init.type = bgfx::RendererType::OpenGL;
-		init.platformData.nwh = glfwGetWin32Window(renderTargetWindow);
-		init.resolution.width = width;
-		init.resolution.height = height;
-		init.resolution.reset = BGFX_RESET_VSYNC;
-		/*GraphicsCallback graphicsCallback;
-		init.callback = &graphicsCallback;*/
+		//initialize vulkan instance/renderer
+		VkApplicationInfo vulkanAppInfo{};
+		vulkanAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		vulkanAppInfo.pApplicationName = "Sekhmet Engine";
+		vulkanAppInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		vulkanAppInfo.pEngineName = "Sekhmet Engine";
+		vulkanAppInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		vulkanAppInfo.apiVersion = VK_API_VERSION_1_0;
 
-		if (!bgfx::init(init))
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		VkInstanceCreateInfo vulkanInstanceCreateInfo{};
+		vulkanInstanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		vulkanInstanceCreateInfo.pApplicationInfo = &vulkanAppInfo;
+		vulkanInstanceCreateInfo.enabledExtensionCount = glfwExtensionCount;
+		vulkanInstanceCreateInfo.ppEnabledExtensionNames = glfwExtensions;
+		vulkanInstanceCreateInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&vulkanInstanceCreateInfo, nullptr, &vulkanInstance) != VK_SUCCESS)
 		{
-			throw std::exception("Failed to initialize BGFX");
+			throw std::exception("Failed to create vulkan instance");
 		}
 
-		//set the background color and enable debug text
-		bgfx::setDebug(BGFX_DEBUG_TEXT);
-		bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355FF, 1.0f, 0);
-		bgfx::setViewRect(0, 0, 0, width, height);
-
-		//loader shaders
-		vertexShader = LoadShader("C:\\code\\c++\\game-engine\\GameEngine\\x64\\Debug\\shaders\\vert.bin");
-		if (vertexShader.idx == bgfx::kInvalidHandle)
-		{
-			throw std::exception("Vertex shader failed to create");
-		}
-
-		fragmentShader = LoadShader("C:\\code\\c++\\game-engine\\GameEngine\\x64\\Debug\\shaders\\frag.bin");
-		if (fragmentShader.idx == bgfx::kInvalidHandle)
-		{
-			throw std::exception("Fragment shader failed to create");
-		}
-
-		programHandle = bgfx::createProgram(vertexShader, fragmentShader, true);
-		if (programHandle.idx == bgfx::kInvalidHandle)
-		{
-			throw std::exception("Program failed to create");
-		}
+		//load shaders
 
 		entities = entitiesIn;
 	}
 
 	void Graphics::Update()
 	{
-		//camera transform
-		const bx::Vec3 at = { 0.0f, 0.0f,  0.0f };
-		const bx::Vec3 eye = { 0.0f, 0.0f, 50.0f };
-		float view[16];
-		bx::mtxLookAt(view, eye, at);
-		float proj[16];
-		bx::mtxProj(proj, 60.0f, float(width) / float(height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
-		bgfx::setViewTransform(0, view, proj);
-
-		bgfx::setViewRect(0, 0, 0, width, height);
-		bgfx::touch(0);
-
-		//object transform
-		float mtx[16];
-		bx::mtxRotateY(mtx, 0.0f);
-		mtx[12] = 0.0f;
-		mtx[13] = 0.0f;
-		mtx[14] = -50.0f;
-		bgfx::setTransform(mtx);
-
-		bgfx::setVertexBuffer(0, vertexBufferHandle);
-		bgfx::setState(BGFX_STATE_DEFAULT);
-		bgfx::submit(0, programHandle);
-		bgfx::dbgTextPrintf(0, 0, 0xF5DE91f, "Welcome to SekhmetEngine");
-		bgfx::frame();
 	}
 
 	void Graphics::Destroy()
 	{
-		bgfx::shutdown();
+		vkDestroyInstance(vulkanInstance, nullptr);
 	}
 
 	void Graphics::ExecuteRange(enki::TaskSetPartition range_, uint32_t threadnum_)
 	{
-		bgfx::touch(0);
 		Render();
 		Update();
 	}
@@ -112,8 +68,6 @@ namespace SekhmetEngine
 	{
 		width = widthIn;
 		height = heightIn;
-
-		bgfx::setViewRect(0, 0, 0, width, height);
 	}
 
 	void Graphics::Render()
@@ -124,7 +78,6 @@ namespace SekhmetEngine
 		std::vector<tinyobj::shape_t> shape = staticMeshComponent->GetModel();
 		tinyobj::attrib_t attributes = staticMeshComponent->GetAttributes();
 		std::vector<float> vertices;
-		std::vector<unsigned int> indices;
 
 		//loop over all vertex entries in the obj
 		for (unsigned int i = 0; i < shape.size(); i++)
@@ -156,17 +109,12 @@ namespace SekhmetEngine
 			}
 		}
 
-		bgfx::VertexLayout vertexLayout;
-		vertexLayout.begin()
-			.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
-			.add(bgfx::Attrib::Normal, 3, bgfx::AttribType::Float)
-			.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float)
-			.end();
+		//Define what the vertex looks like in the vector
 
-		vertexBufferHandle = bgfx::createVertexBuffer(bgfx::makeRef(vertices.data(), vertices.size()), vertexLayout);
+		//create vertex buffer
 	}
 
-	bgfx::ShaderHandle Graphics::LoadShader(std::string shaderPath)
+	void Graphics::LoadShader(std::string shaderPath)
 	{
 		//setup shader file handle with binary format
 		std::ifstream shaderFileHandle(shaderPath, std::ios::ate | std::ios::binary);
@@ -180,13 +128,9 @@ namespace SekhmetEngine
 		shaderFileHandle.read(shaderBinaryData.data(), fileSize);
 
 		//copy the binary data into a bgfx memory handler
-		const bgfx::Memory* shaderMemory = bgfx::copy(shaderBinaryData.data(), fileSize + 1);
 
 		//create a shader handler
-		bgfx::ShaderHandle shaderHandle = bgfx::createShader(shaderMemory);
 
 		shaderFileHandle.close();
-
-		return shaderHandle;
 	}
 }
